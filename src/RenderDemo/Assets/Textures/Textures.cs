854c9361790d7de445d1413b881f035bc880e189
Eric Mellino <erme@microsoft.com>
2016-06-25 12:32:27 -0700

    Fix typo in path affecting one scene

--- src/RenderDemo/Assets/Textures/Textures.cs
+++ src/RenderDemo/Assets/Textures/Textures.cs
@@ -1,17 +1,18 @@
 ﻿using System;
+using System.IO;
 using Veldrid.Graphics;
 
 namespace Veldrid.RenderDemo
 {
     public static class Textures
     {
-        public static readonly ImageProcessorTexture CubeTexture = new ImageProcessorTexture(AppContext.BaseDirectory + "Assets/Textures/CubeTexture.png");
-        public static readonly ImageProcessorTexture WoodTexture = new ImageProcessorTexture(AppContext.BaseDirectory + "Assets/Textures/Wood.png");
+        public static readonly ImageProcessorTexture CubeTexture = new ImageProcessorTexture(Path.Combine(AppContext.BaseDirectory, "Assets/Textures/CubeTexture.png"));
+        public static readonly ImageProcessorTexture WoodTexture = new ImageProcessorTexture(Path.Combine(AppContext.BaseDirectory, "Assets/Textures/Wood.png"));
         public static readonly TextureData PureWhiteTexture = LoadWhiteTextureData();
 
         private static TextureData LoadWhiteTextureData()
         {
-            var texture = new RawTextureDataArray<RgbaFloat>(1, 1, RgbaFloat.SizeInBytes, Graphics.PixelFormat.R32_G32_B32_A32_Float);
+            var texture = new RawTextureDataArray<RgbaFloat>(1, 1, RgbaFloat.SizeInBytes, PixelFormat.R32_G32_B32_A32_Float);
             texture.PixelData[0] = RgbaFloat.White;
             return texture;
         }

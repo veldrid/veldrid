3a3efe642bf606199147b0edb8b3cdf885a946f0
Eric Mellino <erme@microsoft.com>
2017-06-25 15:03:41 -0700

    Better MipmapHelper.GenerateMipmaps

--- src/Veldrid/Graphics/MipmapHelper.cs
+++ src/Veldrid/Graphics/MipmapHelper.cs
@@ -1,24 +1,30 @@
 ﻿using ImageSharp;
 using ImageSharp.PixelFormats;
 using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Veldrid.Graphics
 {
     public static class MipmapHelper
     {
-        public static List<Image<T>> GenerateMipmaps<T>(Image<T> image) where T : struct, IPixel<T>
+        public static Image<T>[] GenerateMipmaps<T>(Image<T> image) where T : struct, IPixel<T>
         {
             int mipLevelCount = ComputeMipLevels(image.Width, image.Height);
-            List<Image<T>> mipLevels = new List<Image<T>>(mipLevelCount);
+            Image<T>[] mipLevels = new Image<T>[mipLevelCount];
+            int i = 0;
             while (image.Width != 1 && image.Height != 1)
             {
                 int newWidth = Math.Max(1, image.Width / 2);
                 int newHeight = Math.Max(1, image.Height / 2);
                 Image<T> newImage = image.Resize(newWidth, newHeight);
-                mipLevels.Add(newImage);
+                Debug.Assert(i < mipLevelCount);
+                mipLevels[i] = newImage;
+
+                i++;
                 image = newImage;
             }
+
+            Debug.Assert(i == mipLevelCount);
 
             return mipLevels;
         }

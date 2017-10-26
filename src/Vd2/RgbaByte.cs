a3bb5409cb44f22b27a041e68c7cef7128ae7565
Eric Mellino <mellinoe@gmail.com>
2017-10-26 10:29:59 -0700

    Add RgbaByte.Pink

--- src/Vd2/RgbaByte.cs
+++ src/Vd2/RgbaByte.cs
@@ -7,6 +7,7 @@
         public static readonly RgbaByte Black = new RgbaByte(0, 0, 0, 255);
         public static readonly RgbaByte White = new RgbaByte(255, 255, 255, 255);
         public static readonly RgbaByte Cyan = new RgbaByte(0, 255, 255, 255);
+        public static readonly RgbaByte Pink = new RgbaByte(255, 155, 191, 255);
 
         public RgbaByte(uint data)
         {

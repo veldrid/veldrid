923997d3d3ca8881a8bb5221bab64d390c0d7b13
Eric Mellino <mellinoe@gmail.com>
2017-12-01 22:51:27 -0800

    Override RgbaFloat.ToString.

--- src/Veldrid/RgbaFloat.cs
+++ src/Veldrid/RgbaFloat.cs
@@ -119,5 +119,10 @@ namespace Veldrid
         {
             return _channels.Equals(other._channels);
         }
+
+        public override string ToString()
+        {
+            return string.Format("R:{0}, G:{1}, B:{2}, A:{3}", R, G, B, A);
+        }
     }
 }

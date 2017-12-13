be10b02b2376c0963d9b81068490d99aad2ddef2
Eric Mellino <mellinoe@gmail.com>
2017-12-13 15:33:58 -0800

    Allow tests to be published self-contained.

---
+++ src/Veldrid.Tests/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static int Main(string[] args)
+    {
+        List<string> newArgs = new List<string>(args);
+        newArgs.Insert(0, typeof(Program).Assembly.Location);
+        int returnCode = Xunit.ConsoleClient.Program.Main(newArgs.ToArray());
+        Console.WriteLine("Tests finished. Press any key to exit.");
+        Console.ReadKey(true);
+        return returnCode;
+    }
+}

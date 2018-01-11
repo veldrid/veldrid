6545a7798e6ac40fc78237bd1a7eeaaae484b722
Eric Mellino <mellinoe@gmail.com>
2018-01-10 19:09:48 -0800

    Add build-packages.sh, same functionality as build-packages.cmd.

---
+++ build-packages.sh
@@ -0,0 +1,11 @@
+dotnet restore src/Veldrid.sln
+dotnet restore src/Veldrid.sln
+
+dotnet pack -c Release src/Veldrid.OpenGLBindings/Veldrid.OpenGLBindings.csproj
+dotnet pack -c Release src/Veldrid.MetalBindings/Veldrid.MetalBindings.csproj
+dotnet pack -c Release src/Veldrid/Veldrid.csproj
+dotnet pack -c Release src/Veldrid.Utilities/Veldrid.Utilities.csproj
+dotnet pack -c Release src/Veldrid.ImGui/Veldrid.ImGui.csproj
+dotnet pack -c Release src/Veldrid.ImageSharp/Veldrid.ImageSharp.csproj
+dotnet pack -c Release src/Veldrid.SDL2/Veldrid.SDL2.csproj
+dotnet pack -c Release src/Veldrid.StartupUtilities/Veldrid.StartupUtilities.csproj

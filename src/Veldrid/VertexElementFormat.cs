5a751e9159c9753b9af4e174625912f5dd1ecf88
Eric Mellino <mellinoe@gmail.com>
2018-01-16 14:35:05 -0800

    Add XML comments to new VertexElementFormat members.

--- src/Veldrid/VertexElementFormat.cs
+++ src/Veldrid/VertexElementFormat.cs
@@ -37,25 +37,85 @@
         /// Four 8-bit unsigned integers.
         /// </summary>
         Byte4,
+        /// <summary>
+        /// Two 8-bit signed normalized integers.
+        /// </summary>
         SByte2_Norm,
+        /// <summary>
+        /// Two 8-bit signed integers.
+        /// </summary>
         SByte2,
+        /// <summary>
+        /// Four 8-bit signed normalized integers.
+        /// </summary>
         SByte4_Norm,
+        /// <summary>
+        /// Four 8-bit signed integers.
+        /// </summary>
         SByte4,
+        /// <summary>
+        /// Two 16-bit unsigned normalized integers.
+        /// </summary>
         UShort2_Norm,
+        /// <summary>
+        /// Two 16-bit unsigned integers.
+        /// </summary>
         UShort2,
+        /// <summary>
+        /// Four 16-bit unsigned normalized integers.
+        /// </summary>
         UShort4_Norm,
+        /// <summary>
+        /// Four 16-bit unsigned integers.
+        /// </summary>
         UShort4,
+        /// <summary>
+        /// Two 16-bit signed normalized integers.
+        /// </summary>
         Short2_Norm,
+        /// <summary>
+        /// Two 16-bit signed integers.
+        /// </summary>
         Short2,
+        /// <summary>
+        /// Four 16-bit signed normalized integers.
+        /// </summary>
         Short4_Norm,
+        /// <summary>
+        /// Four 16-bit signed integers.
+        /// </summary>
         Short4,
+        /// <summary>
+        /// One 32-bit unsigned integer.
+        /// </summary>
         UInt1,
+        /// <summary>
+        /// Two 32-bit unsigned integers.
+        /// </summary>
         UInt2,
+        /// <summary>
+        /// Three 32-bit unsigned integers.
+        /// </summary>
         UInt3,
+        /// <summary>
+        /// Four 32-bit unsigned integers.
+        /// </summary>
         UInt4,
+        /// <summary>
+        /// One 32-bit signed integer.
+        /// </summary>
         Int1,
+        /// <summary>
+        /// Two 32-bit signed integers.
+        /// </summary>
         Int2,
+        /// <summary>
+        /// Three 32-bit signed integers.
+        /// </summary>
         Int3,
+        /// <summary>
+        /// Four 32-bit signed integers.
+        /// </summary>
         Int4,
     }
 }

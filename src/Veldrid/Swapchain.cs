bea4ec051cd8eb62ef468f3bb3ff6f70903590b4
Eric Mellino <mellinoe@gmail.com>
2018-02-23 12:00:46 -0800

    Fill out XML documentation for Swapchain.

--- src/Veldrid/Swapchain.cs
+++ src/Veldrid/Swapchain.cs
@@ -2,12 +2,26 @@
 
 namespace Veldrid
 {
+    /// <summary>
+    /// A device resource providing the ability to present rendered images to a visible surface.
+    /// </summary>
     public abstract class Swapchain : DeviceResource, IDisposable
     {
+        /// <summary>
+        /// Gets a <see cref="Framebuffer"/> representing the render targets of this instance.
+        /// </summary>
         public abstract Framebuffer Framebuffer { get; }
+        /// <summary>
+        /// Resizes the renderable Textures managed by this instance to the given dimensions.
+        /// </summary>
+        /// <param name="width">The new width of the Swapchain.</param>
+        /// <param name="height">The new height of the Swapchain.</param>
         public abstract void Resize(uint width, uint height);
+        /// <summary>
+        /// Gets or sets whether presentation of this Swapchain will be synchronized to the window system's vertical refresh
+        /// rate.
+        /// </summary>
         public abstract bool SyncToVerticalBlank { get; set; }
-
         /// <summary>
         /// A string identifying this instance. Can be used to differentiate between objects in graphics debuggers and other
         /// tools.

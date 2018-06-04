1d3b0116a1f743836fd96c15344b76d02368bb72
Eric Mellino <mellinoe@gmail.com>
2018-06-03 20:34:38 -0700

    Override Equals(object) in RgbaFloat and RgbaByte.

--- src/Veldrid/RgbaByte.cs
+++ src/Veldrid/RgbaByte.cs
@@ -109,6 +109,16 @@ namespace Veldrid
         }
 
         /// <summary>
+        /// Determines whether the specified object is equal to the current object.
+        /// </summary>
+        /// <param name="obj">The object to compare with the current object.</param>
+        /// <returns>true if the specified object is equal to the current object; otherwise, false.</returns>
+        public override bool Equals(object obj)
+        {
+            return obj is RgbaByte other && Equals(other);
+        }
+
+        /// <summary>
         /// Returns the hash code for this instance.
         /// </summary>
         /// <returns>A 32-bit signed integer that is the hash code for this instance.</returns>

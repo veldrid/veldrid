6d0edaf822448e7293d7d7a30ac1cc3c50c734ca
Eric Mellino <mellinoe@gmail.com>
2018-06-05 13:15:50 -0700

    [OpenGL] Don't set new "depth range" device option.

--- src/NeoDemo/NeoDemo.cs
+++ src/NeoDemo/NeoDemo.cs
@@ -48,7 +48,7 @@ namespace Veldrid.NeoDemo
                 WindowInitialState = WindowState.Normal,
                 WindowTitle = "Veldrid NeoDemo"
             };
-            GraphicsDeviceOptions gdOptions = new GraphicsDeviceOptions(false, null, false, ResourceBindingModel.Improved, true);
+            GraphicsDeviceOptions gdOptions = new GraphicsDeviceOptions(false, null, false, ResourceBindingModel.Improved);
 #if DEBUG
             gdOptions.Debug = true;
 #endif

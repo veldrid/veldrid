1a398fa09ee80742572c5125d6c578183538f85f
Eric Mellino <mellinoe@gmail.com>
2018-07-12 20:29:31 -0700

    Make Sdl2Native.LoadFunction public.

--- src/Veldrid.SDL2/Sdl2.cs
+++ src/Veldrid.SDL2/Sdl2.cs
@@ -40,7 +40,15 @@ namespace Veldrid.Sdl2
             return lib;
         }
 
-        private static T LoadFunction<T>(string name)
+        /// <summary>
+        /// Loads an SDL2 function by the given name.
+        /// </summary>
+        /// <typeparam name="T">The delegate type of the function to load.</typeparam>
+        /// <param name="name">The name of the exported native function.</param>
+        /// <returns>A delegate which can be used to invoke the native function.</returns>
+        /// <exception cref="System.InvalidOperationException">Thrown when no function with the given name is exported by SDL2.
+        /// </exception>
+        public static T LoadFunction<T>(string name)
         {
             return s_sdl2Lib.LoadFunction<T>(name);
         }

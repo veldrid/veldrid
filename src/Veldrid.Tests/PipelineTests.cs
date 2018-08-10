065ec9c0288d61168d3b092f9aa9423f3c158332
Eric Mellino <mellinoe@gmail.com>
2018-08-10 13:04:15 -0700

    Add a test for Pipelines created with different InstanceStepRate.

---
+++ src/Veldrid.Tests/PipelineTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Xunit;
+
+namespace Veldrid.Tests
+{
+    public abstract class PipelineTests<T> : GraphicsDeviceTestBase<T> where T : GraphicsDeviceCreator
+    {
+        [Fact]
+        public void CreatePipelines_DifferentInstanceStepRate_Succeeds()
+        {
+            Texture colorTex = RF.CreateTexture(TextureDescription.Texture2D(1, 1, 1, 1, PixelFormat.R8_G8_B8_A8_UNorm, TextureUsage.RenderTarget));
+            Framebuffer framebuffer = RF.CreateFramebuffer(new FramebufferDescription(null, colorTex));
+
+            ShaderSetDescription shaderSet = new ShaderSetDescription(
+                new VertexLayoutDescription[]
+                {
+                    new VertexLayoutDescription(
+                        24,
+                        0,
+                        new VertexElementDescription("Position", VertexElementSemantic.TextureCoordinate, VertexElementFormat.Float2),
+                        new VertexElementDescription("Color_UInt", VertexElementSemantic.TextureCoordinate, VertexElementFormat.UInt4))
+                },
+                TestShaders.LoadVertexFragment(RF, "UIntVertexAttribs"));
+
+            ResourceLayout layout = RF.CreateResourceLayout(new ResourceLayoutDescription(
+                new ResourceLayoutElementDescription("InfoBuffer", ResourceKind.UniformBuffer, ShaderStages.Vertex),
+                new ResourceLayoutElementDescription("Ortho", ResourceKind.UniformBuffer, ShaderStages.Vertex)));
+
+            GraphicsPipelineDescription gpd = new GraphicsPipelineDescription(
+                BlendStateDescription.SingleOverrideBlend,
+                DepthStencilStateDescription.Disabled,
+                RasterizerStateDescription.Default,
+                PrimitiveTopology.PointList,
+                shaderSet,
+                layout,
+                framebuffer.OutputDescription);
+
+            Pipeline pipeline1 = RF.CreateGraphicsPipeline(ref gpd);
+            Pipeline pipeline2 = RF.CreateGraphicsPipeline(ref gpd);
+
+            gpd.ShaderSet.VertexLayouts[0].InstanceStepRate = 4;
+            Pipeline pipeline3 = RF.CreateGraphicsPipeline(ref gpd);
+
+            gpd.ShaderSet.VertexLayouts[0].InstanceStepRate = 5;
+            Pipeline pipeline4 = RF.CreateGraphicsPipeline(ref gpd);
+        }
+    }
+
+#if TEST_OPENGL
+    public class OpenGLPipelineTests : PipelineTests<OpenGLDeviceCreator> { }
+#endif
+#if TEST_OPENGLES
+    public class OpenGLESPipelineTests : PipelineTests<OpenGLESDeviceCreator> { }
+#endif
+#if TEST_VULKAN
+    public class VulkanPipelineTests : PipelineTests<VulkanDeviceCreator> { }
+#endif
+#if TEST_D3D11
+    public class D3D11PipelineTests : PipelineTests<D3D11DeviceCreator> { }
+#endif
+#if TEST_METAL
+        public class MetalPipelineTests : PipelineTests<MetalDeviceCreator> { }
+#endif
+}

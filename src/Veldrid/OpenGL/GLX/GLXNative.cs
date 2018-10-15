6a3337ff2da6ec9ece3197fcc69c9df0514d36b3
Eric Mellino <mellinoe@gmail.com>
2018-10-14 23:30:14 -0700

    Remove unused GLX constants.

--- src/Veldrid/OpenGL/GLX/GLXNative.cs
+++ src/Veldrid/OpenGL/GLX/GLXNative.cs
@@ -8,120 +8,12 @@ namespace Veldrid.OpenGL.GLX
         internal const string GlxLibName = "libGL";
         internal const string X11LibName = "libX11";
 
-        /*
-        * Tokens for glXChooseVisual and glXGetConfig:
-        */
-        internal const int GLX_USE_GL = 1;
-        internal const int GLX_BUFFER_SIZE = 2;
-        internal const int GLX_LEVEL = 3;
-        internal const int GLX_RGBA = 4;
-        internal const int GLX_DOUBLEBUFFER = 5;
-        internal const int GLX_STEREO = 6;
-        internal const int GLX_AUX_BUFFERS = 7;
-        internal const int GLX_RED_SIZE = 8;
-        internal const int GLX_GREEN_SIZE = 9;
-        internal const int GLX_BLUE_SIZE = 10;
-        internal const int GLX_ALPHA_SIZE = 11;
-        internal const int GLX_DEPTH_SIZE = 12;
-        internal const int GLX_STENCIL_SIZE = 13;
-        internal const int GLX_ACCUM_RED_SIZE = 14;
-        internal const int GLX_ACCUM_GREEN_SIZE = 15;
-        internal const int GLX_ACCUM_BLUE_SIZE = 16;
-        internal const int GLX_ACCUM_ALPHA_SIZE = 17;
-
-        /*
-         * Error codes returned by glXGetConfig:
-         */
-        internal const int GLX_BAD_SCREEN = 1;
-        internal const int GLX_BAD_ATTRIBUTE = 2;
-        internal const int GLX_NO_EXTENSION = 3;
-        internal const int GLX_BAD_VISUAL = 4;
-        internal const int GLX_BAD_CONTEXT = 5;
-        internal const int GLX_BAD_VALUE = 6;
-        internal const int GLX_BAD_ENUM = 7;
-
-        /*
-         * GLX 1.1 and later:
-         */
-        internal const int GLX_VENDOR = 1;
-        internal const int GLX_VERSION = 2;
-        internal const int GLX_EXTENSIONS = 3;
-
-        /*
-         * GLX 1.3 and later:
-         */
-        internal const int GLX_CONFIG_CAVEAT = 0x20;
-        internal const int GLX_DONT_CARE = unchecked((int)0xFFFFFFFF);
-        internal const int GLX_X_VISUAL_TYPE = 0x22;
-        internal const int GLX_TRANSPARENT_TYPE = 0x23;
-        internal const int GLX_TRANSPARENT_INDEX_VALUE = 0x24;
-        internal const int GLX_TRANSPARENT_RED_VALUE = 0x25;
-        internal const int GLX_TRANSPARENT_GREEN_VALUE = 0x26;
-        internal const int GLX_TRANSPARENT_BLUE_VALUE = 0x27;
-        internal const int GLX_TRANSPARENT_ALPHA_VALUE = 0x28;
-        internal const int GLX_WINDOW_BIT = 0x00000001;
-        internal const int GLX_PIXMAP_BIT = 0x00000002;
-        internal const int GLX_PBUFFER_BIT = 0x00000004;
-        internal const int GLX_AUX_BUFFERS_BIT = 0x00000010;
-        internal const int GLX_FRONT_LEFT_BUFFER_BIT = 0x00000001;
-        internal const int GLX_FRONT_RIGHT_BUFFER_BIT = 0x00000002;
-        internal const int GLX_BACK_LEFT_BUFFER_BIT = 0x00000004;
-        internal const int GLX_BACK_RIGHT_BUFFER_BIT = 0x00000008;
-        internal const int GLX_DEPTH_BUFFER_BIT = 0x00000020;
-        internal const int GLX_STENCIL_BUFFER_BIT = 0x00000040;
-        internal const int GLX_ACCUM_BUFFER_BIT = 0x00000080;
-        internal const int GLX_NONE = 0x8000;
-        internal const int GLX_SLOW_CONFIG = 0x8001;
-        internal const int GLX_TRUE_COLOR = 0x8002;
-        internal const int GLX_DIRECT_COLOR = 0x8003;
-        internal const int GLX_PSEUDO_COLOR = 0x8004;
-        internal const int GLX_STATIC_COLOR = 0x8005;
-        internal const int GLX_GRAY_SCALE = 0x8006;
-        internal const int GLX_STATIC_GRAY = 0x8007;
-        internal const int GLX_TRANSPARENT_RGB = 0x8008;
-        internal const int GLX_TRANSPARENT_INDEX = 0x8009;
-        internal const int GLX_VISUAL_ID = 0x800B;
-        internal const int GLX_SCREEN = 0x800C;
-        internal const int GLX_NON_CONFORMANT_CONFIG = 0x800D;
-        internal const int GLX_DRAWABLE_TYPE = 0x8010;
-        internal const int GLX_RENDER_TYPE = 0x8011;
-        internal const int GLX_X_RENDERABLE = 0x8012;
-        internal const int GLX_FBCONFIG_ID = 0x8013;
-        internal const int GLX_RGBA_TYPE = 0x8014;
-        internal const int GLX_COLOR_INDEX_TYPE = 0x8015;
-        internal const int GLX_MAX_PBUFFER_WIDTH = 0x8016;
-        internal const int GLX_MAX_PBUFFER_HEIGHT = 0x8017;
-        internal const int GLX_MAX_PBUFFER_PIXELS = 0x8018;
-        internal const int GLX_PRESERVED_CONTENTS = 0x801B;
-        internal const int GLX_LARGEST_PBUFFER = 0x801C;
-        internal const int GLX_WIDTH = 0x801D;
-        internal const int GLX_HEIGHT = 0x801E;
-        internal const int GLX_EVENT_MASK = 0x801F;
-        internal const int GLX_DAMAGED = 0x8020;
-        internal const int GLX_SAVED = 0x8021;
-        internal const int GLX_WINDOW = 0x8022;
-        internal const int GLX_PBUFFER = 0x8023;
-        internal const int GLX_PBUFFER_HEIGHT = 0x8040;
-        internal const int GLX_PBUFFER_WIDTH = 0x8041;
-        internal const int GLX_RGBA_BIT = 0x00000001;
-        internal const int GLX_COLOR_INDEX_BIT = 0x00000002;
-        internal const int GLX_PBUFFER_CLOBBER_MASK = 0x08000000;
-
-        /*
-         * GLX 1.4 and later:
-         */
-        internal const int GLX_SAMPLE_BUFFERS = 0x186a0; /*100000*/
-        internal const int GLX_SAMPLES = 0x186a1; /*100001*/
-
-        internal const int GLX_CONTEXT_DEBUG_BIT_ARB = 0x00000001;
-        internal const int GLX_CONTEXT_FORWARD_COMPATIBLE_BIT_ARB = 0x00000002;
         internal const int GLX_CONTEXT_MAJOR_VERSION_ARB = 0x2091;
         internal const int GLX_CONTEXT_MINOR_VERSION_ARB = 0x2092;
-        internal const int GLX_CONTEXT_FLAGS_ARB = 0x2094;
-        internal const int GLX_CONTEXT_CORE_PROFILE_BIT_ARB = 0x00000001;
-        internal const int GLX_CONTEXT_COMPATIBILITY_PROFILE_BIT_ARB = 0x00000002;
-        internal const int GLX_CONTEXT_ES_PROFILE_BIT_EXT = 0x00000004;
+        
         internal const int GLX_CONTEXT_PROFILE_MASK_ARB = 0x9126;
+        internal const int GLX_CONTEXT_CORE_PROFILE_BIT_ARB = 0x00000001;
+        internal const int GLX_CONTEXT_ES_PROFILE_BIT_EXT = 0x00000004;
 
         [DllImport(GlxLibName)]
         internal static extern XVisualInfo* glXGetVisualFromFBConfig(IntPtr dpy, IntPtr config);

b13734472e46ae7ad0ab2fd65cb70fe3f70e418b
Stephan Vedder <Stephan.Vedder@gmail.com>
2018-10-30 04:26:33 +0100

    Add stream constructor to ImageSharpTexture (#130)
    * Add stream constructor to ImageSharpTexture
    
    * Add extra overload for sRGB

--- src/Veldrid.ImageSharp/ImageSharpTexture.cs
+++ src/Veldrid.ImageSharp/ImageSharpTexture.cs
@@ -4,6 +4,7 @@ using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
 using System;
 using System.Diagnostics;
+using System.IO;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -46,6 +47,9 @@ namespace Veldrid.ImageSharp
         public ImageSharpTexture(string path) : this(Image.Load(path), true) { }
         public ImageSharpTexture(string path, bool mipmap) : this(Image.Load(path), mipmap) { }
         public ImageSharpTexture(string path, bool mipmap, bool srgb) : this(Image.Load(path), mipmap, srgb) { }
+        public ImageSharpTexture(Stream stream) : this(Image.Load(stream), true) { }
+        public ImageSharpTexture(Stream stream, bool mipmap) : this(Image.Load(stream), mipmap) { }
+        public ImageSharpTexture(Stream stream, bool mipmap, bool srgb) : this(Image.Load(stream), mipmap, srgb) { }
         public ImageSharpTexture(Image<Rgba32> image, bool mipmap = true) : this(image, mipmap, false) { }
         public ImageSharpTexture(Image<Rgba32> image, bool mipmap = true, bool srgb = false)
         {

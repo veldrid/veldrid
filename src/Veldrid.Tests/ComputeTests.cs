22c5ffa2f3ba8e341648afa2c932ba6f524fefa9
Eric Mellino <mellinoe@gmail.com>
2018-11-12 01:27:08 -0800

    Add a compute shader test covering buffer offsets.

--- src/Veldrid.Tests/ComputeTests.cs
+++ src/Veldrid.Tests/ComputeTests.cs
@@ -1,13 +1,15 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Xunit;
 
 namespace Veldrid.Tests
 {
-
     [StructLayout(LayoutKind.Sequential)]
-    public struct ComputeTestParams
+    public struct BasicComputeTestParams
     {
         public uint Width;
         public uint Height;
@@ -24,7 +26,7 @@ namespace Veldrid.Tests
             {
                 return;
             }
-            
+
             ResourceLayout layout = RF.CreateResourceLayout(new ResourceLayoutDescription(
                 new ResourceLayoutElementDescription("Params", ResourceKind.UniformBuffer, ShaderStages.Compute),
                 new ResourceLayoutElementDescription("Source", ResourceKind.StructuredBufferReadWrite, ShaderStages.Compute),
@@ -32,11 +34,11 @@ namespace Veldrid.Tests
 
             uint width = 1024;
             uint height = 1024;
-            DeviceBuffer paramsBuffer = RF.CreateBuffer(new BufferDescription((uint)Unsafe.SizeOf<ComputeTestParams>(), BufferUsage.UniformBuffer));
+            DeviceBuffer paramsBuffer = RF.CreateBuffer(new BufferDescription((uint)Unsafe.SizeOf<BasicComputeTestParams>(), BufferUsage.UniformBuffer));
             DeviceBuffer sourceBuffer = RF.CreateBuffer(new BufferDescription(width * height * 4, BufferUsage.StructuredBufferReadWrite, 4));
             DeviceBuffer destinationBuffer = RF.CreateBuffer(new BufferDescription(width * height * 4, BufferUsage.StructuredBufferReadWrite, 4));
 
-            GD.UpdateBuffer(paramsBuffer, 0, new ComputeTestParams { Width = width, Height = height });
+            GD.UpdateBuffer(paramsBuffer, 0, new BasicComputeTestParams { Width = width, Height = height });
 
             float[] sourceData = new float[width * height];
             for (int y = 0; y < height; y++)
@@ -79,6 +81,134 @@ namespace Veldrid.Tests
             GD.Unmap(sourceReadback);
             GD.Unmap(destinationReadback);
         }
+
+        [Theory]
+        [MemberData(nameof(FillBuffer_WithOffsetsData))]
+        public void FillBuffer_WithOffsets(uint srcSetMultiple, uint srcBindingMultiple, uint dstSetMultiple, uint dstBindingMultiple, bool combinedLayout)
+        {
+            if (!GD.Features.ComputeShader) { return; }
+            Debug.Assert((GD.StructuredBufferMinOffsetAlignment % sizeof(uint)) == 0);
+
+            uint valueCount = 512;
+            uint dataSize = valueCount * sizeof(uint);
+            uint totalSrcAlignment = GD.StructuredBufferMinOffsetAlignment * (srcSetMultiple + srcBindingMultiple);
+            uint totalDstAlignment = GD.StructuredBufferMinOffsetAlignment * (dstSetMultiple + dstBindingMultiple);
+
+            DeviceBuffer copySrc = RF.CreateBuffer(
+                new BufferDescription(totalSrcAlignment + dataSize, BufferUsage.StructuredBufferReadOnly, sizeof(uint)));
+            DeviceBuffer copyDst = RF.CreateBuffer(
+                new BufferDescription(totalDstAlignment + dataSize, BufferUsage.StructuredBufferReadWrite, sizeof(uint)));
+
+            ResourceLayout[] layouts;
+            ResourceSet[] sets;
+
+            DeviceBufferRange srcRange = new DeviceBufferRange(copySrc, srcSetMultiple * GD.StructuredBufferMinOffsetAlignment, dataSize);
+            DeviceBufferRange dstRange = new DeviceBufferRange(copyDst, dstSetMultiple * GD.StructuredBufferMinOffsetAlignment, dataSize);
+
+            if (combinedLayout)
+            {
+                layouts = new[]
+                {
+                    RF.CreateResourceLayout(new ResourceLayoutDescription(
+                        new ResourceLayoutElementDescription(
+                            "CopySrc",
+                            ResourceKind.StructuredBufferReadOnly,
+                            ShaderStages.Compute,
+                            ResourceLayoutElementOptions.DynamicBinding),
+                        new ResourceLayoutElementDescription(
+                            "CopyDst",
+                            ResourceKind.StructuredBufferReadWrite,
+                            ShaderStages.Compute,
+                            ResourceLayoutElementOptions.DynamicBinding)))
+                };
+                sets = new[]
+                {
+                    RF.CreateResourceSet(new ResourceSetDescription(layouts[0], srcRange, dstRange))
+                };
+            }
+            else
+            {
+                layouts = new[]
+                {
+                    RF.CreateResourceLayout(new ResourceLayoutDescription(
+                        new ResourceLayoutElementDescription(
+                            "CopySrc",
+                            ResourceKind.StructuredBufferReadOnly,
+                            ShaderStages.Compute,
+                            ResourceLayoutElementOptions.DynamicBinding))),
+                    RF.CreateResourceLayout(new ResourceLayoutDescription(
+                        new ResourceLayoutElementDescription(
+                            "CopyDst",
+                            ResourceKind.StructuredBufferReadWrite,
+                            ShaderStages.Compute,
+                            ResourceLayoutElementOptions.DynamicBinding)))
+                };
+                sets = new[]
+                {
+                    RF.CreateResourceSet(new ResourceSetDescription(layouts[0], srcRange)),
+                    RF.CreateResourceSet(new ResourceSetDescription(layouts[1], dstRange)),
+                };
+            }
+
+            Pipeline pipeline = RF.CreateComputePipeline(new ComputePipelineDescription(
+                TestShaders.LoadCompute(RF, combinedLayout ? "FillBuffer" : "FillBuffer_SeparateLayout"),
+                layouts,
+                1, 1, 1));
+
+            uint[] srcData = Enumerable.Range(0, (int)copySrc.SizeInBytes / sizeof(uint)).Select(i => (uint)i).ToArray();
+            GD.UpdateBuffer(copySrc, 0, srcData);
+
+            CommandList cl = RF.CreateCommandList();
+            cl.Begin();
+            cl.SetPipeline(pipeline);
+            if (combinedLayout)
+            {
+                uint[] offsets = new[]
+                {
+                    srcBindingMultiple * GD.StructuredBufferMinOffsetAlignment,
+                    dstBindingMultiple * GD.StructuredBufferMinOffsetAlignment
+                };
+                cl.SetComputeResourceSet(0, sets[0], offsets);
+            }
+            else
+            {
+                uint offset = srcBindingMultiple * GD.StructuredBufferMinOffsetAlignment;
+                cl.SetComputeResourceSet(0, sets[0], 1, ref offset);
+                offset = dstBindingMultiple * GD.StructuredBufferMinOffsetAlignment;
+                cl.SetComputeResourceSet(1, sets[1], 1, ref offset);
+            }
+            cl.Dispatch(512, 1, 1);
+            cl.End();
+            GD.SubmitCommands(cl);
+            GD.WaitForIdle();
+
+            DeviceBuffer readback = GetReadback(copyDst);
+
+            MappedResourceView<uint> readView = GD.Map<uint>(readback, MapMode.Read);
+            for (uint i = 0; i < valueCount; i++)
+            {
+                uint srcIndex = totalSrcAlignment / sizeof(uint) + i;
+                uint expected = srcData[(int)srcIndex];
+
+                uint dstIndex = totalDstAlignment / sizeof(uint) + i;
+                uint actual = readView[dstIndex];
+
+                Assert.Equal(expected, actual);
+            }
+            GD.Unmap(readback);
+        }
+
+        public static IEnumerable<object[]> FillBuffer_WithOffsetsData()
+        {
+            foreach (uint srcSetMultiple in new[] { 0, 2, 10 })
+                foreach (uint srcBindingMultiple in new[] { 0, 2, 10 })
+                    foreach (uint dstSetMultiple in new[] { 0, 2, 10 })
+                        foreach (uint dstBindingMultiple in new[] { 0, 2, 10 })
+                            foreach (bool combinedLayout in new[] { false, true })
+                            {
+                                yield return new object[] { srcSetMultiple, srcBindingMultiple, dstSetMultiple, dstBindingMultiple, combinedLayout };
+                            }
+        }
     }
 
 #if TEST_OPENGL
@@ -91,7 +221,7 @@ namespace Veldrid.Tests
     public class VulkanComputeTests : ComputeTests<VulkanDeviceCreatorWithMainSwapchain> { }
 #endif
 #if TEST_D3D11
-    public class D3D11ComputeTests : ComputeTests<D3D11DeviceCreator> { }
+    public class D3D11ComputeTests : ComputeTests<D3D11DeviceCreatorWithMainSwapchain> { }
 #endif
 #if TEST_METAL
         public class MetalComputeTests : RenderTests<MetalDeviceCreator> { }

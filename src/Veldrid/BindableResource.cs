a7710a5c7773e626282e97539f212c5653c6a816
Eric Mellino <mellinoe@gmail.com>
2018-12-15 13:11:27 -0800

    Include extra types in XML documentation for BindableResource.

--- src/Veldrid/BindableResource.cs
+++ src/Veldrid/BindableResource.cs
@@ -2,7 +2,8 @@
 {
     /// <summary>
     /// A resource which can be bound in a <see cref="ResourceSet"/> and used in a shader.
-    /// See <see cref="DeviceBuffer"/>, <see cref="TextureView"/> and <see cref="Sampler"/>.
+    /// See <see cref="DeviceBuffer"/>, <see cref="DeviceBufferRange"/>, <see cref="Texture"/>, <see cref="TextureView"/>
+    /// and <see cref="Sampler"/>.
     /// </summary>
     public interface BindableResource { }
 }

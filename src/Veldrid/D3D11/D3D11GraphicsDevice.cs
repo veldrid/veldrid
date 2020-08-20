84adf9723c27d35c8d22e863ba460cd2d85d68c6
Stephan Vedder <vedder@mbits.info>
2020-08-20 09:42:26 +0200

    Revert feature level order

--- src/Veldrid/D3D11/D3D11GraphicsDevice.cs
+++ src/Veldrid/D3D11/D3D11GraphicsDevice.cs
@@ -77,7 +77,11 @@ namespace Veldrid.D3D11
                     Vortice.Direct3D11.D3D11.D3D11CreateDevice(_dxgiAdapter,
                         Vortice.Direct3D.DriverType.Hardware,
                         flags,
-                        new [] { Vortice.Direct3D.FeatureLevel.Level_11_0, Vortice.Direct3D.FeatureLevel.Level_11_1, },
+                        new[] 
+                        { 
+                            Vortice.Direct3D.FeatureLevel.Level_11_1, 
+                            Vortice.Direct3D.FeatureLevel.Level_11_0, 
+                        },
                         out _device).CheckError();
                 }
                 else
@@ -85,7 +89,11 @@ namespace Veldrid.D3D11
                     Vortice.Direct3D11.D3D11.D3D11CreateDevice(null,
                         Vortice.Direct3D.DriverType.Hardware,
                         flags,
-                        new[] { Vortice.Direct3D.FeatureLevel.Level_11_0, Vortice.Direct3D.FeatureLevel.Level_11_1, },
+                        new[] 
+                        { 
+                            Vortice.Direct3D.FeatureLevel.Level_11_1, 
+                            Vortice.Direct3D.FeatureLevel.Level_11_0, 
+                        },
                         out _device).CheckError();
                 }
             }

23ca49f580343b354208037b318859913ef850de
Callum Tolley <callum.tolley@ocado.com>
2020-09-22 11:04:53 +0100

    Add SDL_Rect definition

--- src/Veldrid.SDL2/Sdl2.Window.cs
+++ src/Veldrid.SDL2/Sdl2.Window.cs
@@ -238,4 +238,12 @@ namespace Veldrid.Sdl2
         public int refresh_rate;
         public void* driverdata;
     }
+
+    public struct SDL_Rect
+    {
+        public int x;
+        public int y;
+        public int w;
+        public int h;
+    }
 }

151e825a8191a35ba821692f2408ce76880883e6
SupinePandora43 <supinepandoraggg@gmail.com>
2021-03-04 13:25:30 +0500

    Specify NativeLibrary

--- src/Veldrid/MTL/MTLGraphicsDevice.cs
+++ src/Veldrid/MTL/MTLGraphicsDevice.cs
@@ -6,6 +6,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Threading;
 using NativeLibraryLoader;
+using NativeLibrary = NativeLibraryLoader.NativeLibrary;
 using Veldrid.MetalBindings;
 
 namespace Veldrid.MTL

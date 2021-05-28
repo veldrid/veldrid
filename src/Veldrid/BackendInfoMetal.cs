f52243bb62ab6d36e453c82728474af358652aac
TechPizza <piatkowski2002@gmail.com>
2021-05-28 23:05:31 +0200

    Added BackendInfoMetal

---
+++ src/Veldrid/BackendInfoMetal.cs
@@ -0,0 +1,23 @@
+﻿#if !EXCLUDE_METAL_BACKEND
+using Veldrid.MTL;
+
+namespace Veldrid
+{
+    /// <summary>
+    /// Exposes Metal-specific functionality,
+    /// useful for interoperating with native components which interface directly with Metal.
+    /// Can only be used on <see cref="GraphicsBackend.Metal"/>.
+    /// </summary>
+    public class BackendInfoMetal
+    {
+        private readonly MTLGraphicsDevice _gd;
+
+        internal BackendInfoMetal(MTLGraphicsDevice gd)
+        {
+            _gd = gd;
+        }
+
+        public MTLFeatureSupport FeatureSupport => _gd.MetalFeatures;
+    }
+}
+#endif

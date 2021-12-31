b543e4bfc246e7c293d231de8eb66689764c14b1
NtFreX <frederik@traber.org>
2021-12-31 12:12:58 +0100

    add can parse mtllib tests for ObjParser

---
+++ src/Veldrid.Tests/ObjFileTests.cs
@@ -0,0 +1,17 @@
+﻿using Veldrid.Utilities;
+using Xunit;
+
+namespace Veldrid.Tests
+{
+    public class ObjFileTests
+    {
+        [Theory]
+        [InlineData("space_station.mtl")]
+        [InlineData("space station.mtl")]
+        public void CanParseMtllib(string mtllib)
+        {
+            var objFile = new ObjParser().Parse(new string[] { $"mtllib {mtllib}" });
+            Assert.Equal(mtllib, objFile.MaterialLibName);
+        }
+    }
+}

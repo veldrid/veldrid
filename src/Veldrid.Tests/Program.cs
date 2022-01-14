064376f7be6aa710f7fa7437d5e0a9ee634ef693
Martyn York <etinquis@gmail.com>
2022-01-14 12:42:20 -0800

    revert Tests Program.cs

--- src/Veldrid.Tests/Program.cs
+++ src/Veldrid.Tests/Program.cs
@@ -1,14 +1,12 @@
 using System;
 using System.Collections.Generic;
-using Veldrid.Tests;
 
 class Program
 {
     static int Main(string[] args)
     {
         List<string> newArgs = new List<string>(args);
-        newArgs.InsertRange(0, new[] { "-parallel", "none" });
-        newArgs.Insert(0, typeof(BufferTestBase<>).Assembly.Location);
+        newArgs.Insert(0, typeof(Program).Assembly.Location);
         int returnCode = Xunit.ConsoleClient.Program.Main(newArgs.ToArray());
         Console.WriteLine("Tests finished. Press any key to exit.");
         if (!Console.IsInputRedirected)

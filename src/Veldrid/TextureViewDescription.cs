4d25d41c363738a2a039a42281231d1f576c0a01
Amer Koleci <amerkoleci@gmail.com>
2022-02-17 11:58:37 +0100

    Revert constructors of TextureViewDescription to keep API backward compatible.

--- src/Veldrid/TextureViewDescription.cs
+++ src/Veldrid/TextureViewDescription.cs
@@ -45,8 +45,24 @@ namespace Veldrid
         /// </summary>
         /// <param name="target">The desired target <see cref="Texture"/>. This <see cref="Texture"/> must have been created
         /// with the <see cref="TextureUsage.Sampled"/> usage flag.</param>
+        public TextureViewDescription(Texture target)
+        {
+            Target = target;
+            ViewType = GetFromTexture(target);
+            BaseMipLevel = 0;
+            MipLevels = target.MipLevels;
+            BaseArrayLayer = 0;
+            ArrayLayers = target.ArrayLayers;
+            Format = target.Format;
+        }
+
+        /// <summary>
+        /// Constructs a new TextureViewDescription.
+        /// </summary>
+        /// <param name="target">The desired target <see cref="Texture"/>. This <see cref="Texture"/> must have been created
+        /// with the <see cref="TextureUsage.Sampled"/> usage flag.</param>
         /// <param name="viewType">The view type to create or <see cref="TextureViewType.Undefined"/> to detect from texture.</param>
-        public TextureViewDescription(Texture target, TextureViewType viewType = TextureViewType.Undefined)
+        public TextureViewDescription(Texture target, TextureViewType viewType)
         {
             Target = target;
             ViewType = GetFromTexture(viewType, target, target.Usage);
@@ -66,8 +82,28 @@ namespace Veldrid
         /// This format must be "compatible" with the target Texture's. For uncompressed formats, the overall size and number of
         /// components in this format must be equal to the underlying format. For compressed formats, it is only possible to use
         /// the same PixelFormat or its sRGB/non-sRGB counterpart.</param>
+        public TextureViewDescription(Texture target, PixelFormat format)
+        {
+            Target = target;
+            ViewType = GetFromTexture(target);
+            BaseMipLevel = 0;
+            MipLevels = target.MipLevels;
+            BaseArrayLayer = 0;
+            ArrayLayers = target.ArrayLayers;
+            Format = format;
+        }
+
+        /// <summary>
+        /// Constructs a new TextureViewDescription.
+        /// </summary>
+        /// <param name="target">The desired target <see cref="Texture"/>. This <see cref="Texture"/> must have been created
+        /// with the <see cref="TextureUsage.Sampled"/> usage flag.</param>
+        /// <param name="format">Specifies how the data within the target Texture will be viewed.
+        /// This format must be "compatible" with the target Texture's. For uncompressed formats, the overall size and number of
+        /// components in this format must be equal to the underlying format. For compressed formats, it is only possible to use
+        /// the same PixelFormat or its sRGB/non-sRGB counterpart.</param>
         /// <param name="viewType">The view type to create or <see cref="TextureViewType.Undefined"/> to detect from texture.</param>
-        public TextureViewDescription(Texture target, PixelFormat format, TextureViewType viewType = TextureViewType.Undefined)
+        public TextureViewDescription(Texture target, PixelFormat format, TextureViewType viewType)
         {
             Target = target;
             ViewType = GetFromTexture(viewType, target, target.Usage);
@@ -87,8 +123,28 @@ namespace Veldrid
         /// <param name="mipLevels">The number of mip levels visible in the view.</param>
         /// <param name="baseArrayLayer">The base array layer visible in the view.</param>
         /// <param name="arrayLayers">The number of array layers visible in the view.</param>
+        public TextureViewDescription(Texture target, uint baseMipLevel, uint mipLevels, uint baseArrayLayer, uint arrayLayers)
+        {
+            Target = target;
+            ViewType = GetFromTexture(target);
+            BaseMipLevel = baseMipLevel;
+            MipLevels = mipLevels;
+            BaseArrayLayer = baseArrayLayer;
+            ArrayLayers = arrayLayers;
+            Format = target.Format;
+        }
+
+        /// <summary>
+        /// Constructs a new TextureViewDescription.
+        /// </summary>
+        /// <param name="target">The desired target <see cref="Texture"/>.</param>
+        /// <param name="baseMipLevel">The base mip level visible in the view. Must be less than <see cref="Texture.MipLevels"/>.
+        /// </param>
+        /// <param name="mipLevels">The number of mip levels visible in the view.</param>
+        /// <param name="baseArrayLayer">The base array layer visible in the view.</param>
+        /// <param name="arrayLayers">The number of array layers visible in the view.</param>
         /// <param name="viewType">The view type to create or <see cref="TextureViewType.Undefined"/> to detect from texture.</param>
-        public TextureViewDescription(Texture target, uint baseMipLevel, uint mipLevels, uint baseArrayLayer, uint arrayLayers, TextureViewType viewType = TextureViewType.Undefined)
+        public TextureViewDescription(Texture target, uint baseMipLevel, uint mipLevels, uint baseArrayLayer, uint arrayLayers, TextureViewType viewType)
         {
             Target = target;
             ViewType = GetFromTexture(viewType, target, target.Usage);
@@ -112,8 +168,32 @@ namespace Veldrid
         /// <param name="mipLevels">The number of mip levels visible in the view.</param>
         /// <param name="baseArrayLayer">The base array layer visible in the view.</param>
         /// <param name="arrayLayers">The number of array layers visible in the view.</param>
+        public TextureViewDescription(Texture target, PixelFormat format, uint baseMipLevel, uint mipLevels, uint baseArrayLayer, uint arrayLayers)
+        {
+            Target = target;
+            ViewType = GetFromTexture(target);
+            BaseMipLevel = baseMipLevel;
+            MipLevels = mipLevels;
+            BaseArrayLayer = baseArrayLayer;
+            ArrayLayers = arrayLayers;
+            Format = format;
+        }
+
+        /// <summary>
+        /// Constructs a new TextureViewDescription.
+        /// </summary>
+        /// <param name="target">The desired target <see cref="Texture"/>.</param>
+        /// <param name="format">Specifies how the data within the target Texture will be viewed.
+        /// This format must be "compatible" with the target Texture's. For uncompressed formats, the overall size and number of
+        /// components in this format must be equal to the underlying format. For compressed formats, it is only possible to use
+        /// the same PixelFormat or its sRGB/non-sRGB counterpart.</param>
+        /// <param name="baseMipLevel">The base mip level visible in the view. Must be less than <see cref="Texture.MipLevels"/>.
+        /// </param>
+        /// <param name="mipLevels">The number of mip levels visible in the view.</param>
+        /// <param name="baseArrayLayer">The base array layer visible in the view.</param>
+        /// <param name="arrayLayers">The number of array layers visible in the view.</param>
         /// <param name="viewType">The view type to create or <see cref="TextureViewType.Undefined"/> to detect from texture.</param>
-        public TextureViewDescription(Texture target, PixelFormat format, uint baseMipLevel, uint mipLevels, uint baseArrayLayer, uint arrayLayers, TextureViewType viewType = TextureViewType.Undefined)
+        public TextureViewDescription(Texture target, PixelFormat format, uint baseMipLevel, uint mipLevels, uint baseArrayLayer, uint arrayLayers, TextureViewType viewType)
         {
             Target = target;
             ViewType = GetFromTexture(viewType, target, target.Usage);

67c0eb62f17c629bce6b193237befc7edc761c6c
TechPizza <23627133+TechnologicalPizza@users.noreply.github.com>
2022-08-23 20:45:04 +0200

    Updated xunit

--- src/Veldrid.Tests/RenderTests.cs
+++ src/Veldrid.Tests/RenderTests.cs
@@ -1301,7 +1301,7 @@ namespace Veldrid.Tests
                     float yComp = y * readback.Width;
                     float val = (yComp + xComp) / (readback.Width * readback.Height);
 
-                    Assert.Equal(val, readView[x, y], 2);
+                    Assert.Equal(val, readView[x, y], 2f);
                 }
             }
             GD.Unmap(readback);
